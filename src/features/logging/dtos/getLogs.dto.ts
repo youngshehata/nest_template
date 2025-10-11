5b2901082ea4aa18500a3cc5b58efa1bed70bb86
Ahmed Shehata <68691316+youngshehata@users.noreply.github.com>
2025-10-11 20:15:24 +0300

    enhance: getLogs endpoint

--- src/features/logging/dtos/getLogs.dto.ts
+++ src/features/logging/dtos/getLogs.dto.ts
@@ -3,24 +3,29 @@ import {
   CONST_DEFAULT_PAGE_SIZE,
 } from '@app/common/constraints/pagination/pagination.constraints';
 import { TLog } from '@app/common/types/TLog';
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsOptional, IsPositive } from 'class-validator';
 
 export class GetLogsDto {
+  @ApiProperty()
   @IsDate()
   @IsNotEmpty()
   @Transform(({ value }) => new Date(value))
   startDate: Date;
 
+  @ApiProperty()
   @IsDate()
   @IsNotEmpty()
   @Transform(({ value }) => new Date(value))
   endDate: Date;
 
+  @ApiProperty({ nullable: true, default: CONST_DEFAULT_PAGE })
   @IsOptional()
   @IsPositive()
   page?: number = CONST_DEFAULT_PAGE;
 
+  @ApiProperty({ nullable: true, default: CONST_DEFAULT_PAGE_SIZE })
   @IsOptional()
   @IsPositive()
   pageSize?: number = CONST_DEFAULT_PAGE_SIZE;
--- src/features/logging/logging.controller.ts
+++ src/features/logging/logging.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { LoggingService } from './logging.service';
 import { GetLogsDto } from './dtos/getLogs.dto';
+import { FastifyReply } from 'fastify';
 
 @Controller('logging')
 export class LoggingController {
   constructor(private readonly loggingService: LoggingService) {}
   @Post('find')
-  async getLogs(@Body() data: GetLogsDto) {
-    return this.loggingService.getLogs(data);
+  async getLogs(@Body() data: GetLogsDto, @Res() res: FastifyReply) {
+    const response = await this.loggingService.getLogs(data);
+    return res.status(200).send(response);
   }
 }
