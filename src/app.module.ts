d0fa68cc1f8bc62c1aa86a28c931b6060ba772d4
Ahmed Shehata <68691316+a-shehata-dev@users.noreply.github.com>
2025-11-13 18:21:18 +0200

    Revert "feat: Upload Service"
    This reverts commit c57c9703a426612f27c4091d567e41e9c1a7f23a.

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,6 @@ import { AuthGuard } from './common/guards/auth.guard';
 import { JwtService } from '@nestjs/jwt';
 import { RolesModule } from './features/roles/roles.module';
 import { PrismaService } from 'prisma/prisma.service';
-import { UploadService } from './common/services/upload.service';
 
 @Module({
   imports: [
@@ -33,7 +32,6 @@ import { UploadService } from './common/services/upload.service';
     AppService,
     JwtService,
     PrismaService,
-    UploadService,
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ import { fastifyCookie } from '@fastify/cookie';
 import { RolesGuard } from './common/guards/roles.guard';
 import { RolesService } from './features/roles/roles.service';
 import { UsersService } from './features/users/users.service';
-import fastifyMultipart from '@fastify/multipart';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
@@ -26,13 +25,8 @@ async function bootstrap() {
   //! ############ Helmet ############
   await app.register(helmet);
 
-  //! ############ Fastify ############
+  //! ############ Cookie ############
   app.register(fastifyCookie as any);
-  app.register(fastifyMultipart as any, {
-    limits: {
-      fileSize: 50 * 1024 * 1024, // 50 MB limit
-    },
-  });
 
   //! ############ Logger ############
   app.useLogger(
